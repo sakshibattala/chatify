5bceac4119e7752fe312a726274234a08ea3f921
Sakshi <sakshibattala6@gmail.com>
2026-02-10 11:24:38 +0530

    configure backend to serve React build and enable production setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,25 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
+const __dirname = path.resolve();
+
 app.use("/auth", authRoutes);
 
+// Production mode: serve React
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../frontend/dist");
+  app.use(express.static(frontendPath));
+
+  // Serve index.html for any unknown route
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("app is listening on port: ", PORT));
+app.listen(PORT, () => console.log("app is listening on port:", PORT));
